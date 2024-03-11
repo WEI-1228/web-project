c782c4f53bab4a9a283623f14021b84dc9df488b
解矣 <516363082@qq.com>
2024-03-11 13:27:03 +0800

    增加了hello接口测试test.http

--- src/main/java/cn/anlper/wiki/controller/TestController.java
+++ src/main/java/cn/anlper/wiki/controller/TestController.java
@@ -1,6 +1,8 @@
 package cn.anlper.wiki.controller;
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
@@ -8,8 +10,13 @@ public class TestController {
 //    @RequestMapping(value = "/hello/1", method = RequestMethod.GET)
 //    @GetMapping("/hello")
 //    @PostMapping("/hello/1")
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
     public String hello() {
         return "Hello World!";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name) {
+        return "Hello World! Post, " + name;
     }
 }
