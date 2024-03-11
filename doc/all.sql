866089eafe1f3f38a82d5ae590364ac77c978a37
解矣 <516363082@qq.com>
2024-03-11 15:37:25 +0800

    3-5 集成Mybatis generator，生成demo表代码

--- doc/all.sql
+++ doc/all.sql
@@ -6,3 +6,12 @@ create table `test` (
     primary key (`id`)
 );
 insert into `test` values (1, '测试', 'password')
+
+drop table if exists `demo`;
+create table `demo` (
+    `id` bigint not null comment 'id',
+    `name` varchar(50) comment '名称',
+    primary key (`id`)
+);
+
+insert into `demo` values (1, '测试')
