1936d86d3e71fbcc7e5c4fb867f881594ff1391e
解矣 <516363082@qq.com>
2024-03-11 13:46:53 +0800

    增加了自定义配置项，并通过Value注解的方式进行读取

--- src/main/java/cn/anlper/wiki/controller/TestController.java
+++ src/main/java/cn/anlper/wiki/controller/TestController.java
@@ -1,18 +1,21 @@
 package cn.anlper.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    @Value("${test.hello:TEST}")
+    private String testHello;
 
 //    @RequestMapping(value = "/hello/1", method = RequestMethod.GET)
 //    @GetMapping("/hello")
 //    @PostMapping("/hello/1")
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World!";
+        return "Hello World!" + testHello;
     }
 
     @PostMapping("/hello/post")
