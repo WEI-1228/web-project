b3cf4c54c1bdac7b36bba48db7abb411fbe126a3
解矣 <516363082@qq.com>
2024-03-11 16:24:20 +0800

    3-6 增加通用返回类CommonResp，以后所有接口返回值全都是CommonResp类型

--- src/main/java/cn/anlper/wiki/controller/EbookController.java
+++ src/main/java/cn/anlper/wiki/controller/EbookController.java
@@ -1,6 +1,7 @@
 package cn.anlper.wiki.controller;
 
 import cn.anlper.wiki.domain.Ebook;
+import cn.anlper.wiki.resp.CommonResp;
 import cn.anlper.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +17,10 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping("/list")
-    public List<Ebook> list() {
-        return ebookService.list();
+    public CommonResp list() {
+        CommonResp<List<Ebook>> resp = new CommonResp<>();
+        List<Ebook> list = ebookService.list();
+        resp.setContent(list);
+        return resp;
     }
 }
