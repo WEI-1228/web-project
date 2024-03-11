77808fce7b1487bc554f5f3151f8e75c92b1b47e
解矣 <516363082@qq.com>
2024-03-11 12:56:57 +0800

    修改启动文案

---
+++ src/main/java/cn/anlper/WikiApplication.java
@@ -0,0 +1,17 @@
+package cn.anlper;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+@SpringBootApplication
+public class WikiApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
+    public static void main(String[] args) {
+        ConfigurableEnvironment environment = SpringApplication.run(WikiApplication.class, args).getEnvironment();
+        LOG.info("启动成功！！");
+        LOG.info("地址：\thttp://127.0.0.1:{}", environment.getProperty("server.port"));
+    }
+}
