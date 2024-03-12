1333295256b12b648bd3a0d46937871c4c30c5e9
解矣 <516363082@qq.com>
2024-03-12 23:03:32 +0800

    5-2 使用axios校验接口是否正常，添加了跨域访问规则

---
+++ src/main/java/cn/anlper/wiki/config/CorsConfig.java
@@ -0,0 +1,19 @@
+package cn.anlper.wiki.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedHeaders(CorsConfiguration.ALL)
+                .allowedMethods(CorsConfiguration.ALL)
+                .allowCredentials(true)
+                .maxAge(3600); // 1小时内不需要再预检（发OPTIONS请求）。前端发送请求之前会偷偷发送一个OPTIONS请求，检查是否能连接成功。
+    }
+}
