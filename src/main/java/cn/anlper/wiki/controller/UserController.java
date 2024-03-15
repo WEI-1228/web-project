e504bad8aa7ee1d2eb08bea569b11b0a3810ff33
解矣 <516363082@qq.com>
2024-03-15 16:05:49 +0800

    9-4 增加Redis保存登陆token

--- src/main/java/cn/anlper/wiki/controller/UserController.java
+++ src/main/java/cn/anlper/wiki/controller/UserController.java
@@ -9,18 +9,33 @@ import cn.anlper.wiki.resp.PageResp;
 import cn.anlper.wiki.resp.UserLoginResp;
 import cn.anlper.wiki.resp.UserQueryResp;
 import cn.anlper.wiki.service.UserService;
+import cn.anlper.wiki.util.SnowFlake;
+import com.alibaba.fastjson.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.validation.Valid;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(UserController.class);
+
     @Resource
     private UserService userService;
+
+    @Resource
+    private RedisTemplate redisTemplate;
+
+    @Resource
+    private SnowFlake snowFlake;
 
     @GetMapping("/list")
     public CommonResp<PageResp<UserQueryResp>> list(@Valid UserQueryReq req) {
@@ -66,6 +81,14 @@ public class UserController {
         req.setPassword(DigestUtils.md5DigestAsHex(req.getPassword().getBytes()));
         CommonResp<UserLoginResp> resp = new CommonResp<>();
         UserLoginResp userLoginResp = userService.login(req);
+
+        // 生成单点登陆token，并放入redis中
+        Long token = snowFlake.nextId();
+        LOG.info("生成单点登陆token：{}", token);
+        redisTemplate.opsForValue().set(token, JSONObject.toJSONString(userLoginResp), 3600 * 24, TimeUnit.SECONDS);
+        userLoginResp.setToken(token.toString());
+
+
         resp.setContent(userLoginResp);
         return resp;
     }
